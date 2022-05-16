49fd26af3b32cb5fbf2fa5212a4c1551556eb210
Claudio Hernandez <93549233+HernandezAriel@users.noreply.github.com>
2022-05-16 01:43:57 -0300

    Add files via upload

---
+++ Challenge/src/main/java/com/pair/emails/Email.java
@@ -0,0 +1,14 @@
+package com.pair.Email;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Email {
+    private String email;
+    private String content;
+    private String subject;
+}
